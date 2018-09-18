299c13c0b0b522a253e3ea2800a6af8aced56046
jonathanramirez97 <42975560+jonathanramirez97@users.noreply.github.com>
2018-09-17 22:17:10 -0400

    clase compra cont

--- MAKITA/src/proyecto/Compra.java
+++ MAKITA/src/proyecto/Compra.java
@@ -11,3 +11,13 @@ public class Compra {
     private int IdProveedor ;
     private int IdEmpleado ;
     private LeerConsola leerConsola;
+
+public Compra()
+{
+	IdCompra=0;
+    Fecha="";
+    MontoTotal =0;
+    IdProveedor =0;
+    IdEmpleado =0;
+    leerConsola= new LeerConsola();
+}
