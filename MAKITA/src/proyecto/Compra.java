95b5b9cc2bbab04bf8748a013553df87992fcebb
jonathanramirez97 <42975560+jonathanramirez97@users.noreply.github.com>
2018-09-17 22:03:56 -0400

    subo inicio clase compra

---
+++ MAKITA/src/proyecto/Compra.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Compra {
+	
+	private int IdCompra;
+    private String Fecha;
+    private int MontoTotal ;
+    private int IdProveedor ;
+    private int IdEmpleado ;
+    private LeerConsola leerConsola;
